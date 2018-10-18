98f782a4fd8eef9da782b2c46963dad421d8d964
Mese Bilisim <mesebilisimdestek@gmail.com>
2018-10-18 19:23:23 +0300

    Create _7.c

---
+++ _7.c
@@ -0,0 +1,17 @@
+
+# include "stdio.h"
+
+
+int toplama(int i, int j){
+	return i+j;
+}
+
+void main()
+{
+	toplama(1, 2);
+	char i, j;
+	i='h';
+	armut=4;
+
+	printf("toplama öçşğü : %d\n", i+armut);
+}
